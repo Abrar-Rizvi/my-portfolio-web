7049679f55415636005dc5e0561634b773a596cb
Muhammad Abrar <abrar8949@gmail.com>
2024-10-29 21:01:11 -0700

    changed some color

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -6,18 +6,20 @@ const Feature = () => {
   return (
     <div id="featured-container "  className='bg-black p-10 mt-10' >
     <h2 className='text-center text-white text-3xl font-bold mt-[50px] uppercase
-    sm:text-yellow-400 md:text-blue-500  lg:text-gray-600 xl:text-red-600
+    
     '>creative Projects</h2>
-    <div id="cards" className='mt-[50px] flex flex-col space-y-4 '>
+    <div id="cards" className=' mt-[50px] flex flex-col space-y-10 '>
      
         {cards.map((card, idx) => {
-          return <div key={idx} className="card-of-image-description w-full lg:w-[60%] xl:w-[80%]  shadow-lg mx-auto   ">
+          return <div key={idx} className="card-of-image-description group w-full lg:w-[60%] xl:w-[80%] space-y-10  shadow-lg mx-auto hover:scale-110 transition-all  ease-in-out duration-700  ">
             <Link id="link" href={card.href}>
             <Image id="img" src={card.image} width={200} height={200} alt="card image" 
             className='w-full h-full'
             />
             </Link>
-            <h1 className='text-white border-b  text-center text-2xl mt-2 font-semibold'>{card.hone}</h1>
+            <h1 className='text-white border-b  text-center text-xl mt-2 font-medium group-hover:text-yellow-400
+            transition-all duration-300
+            ' >{card.hone}</h1>
 
           </div>
         }) }
